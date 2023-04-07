f11bc966e01eafcbae3cb7974b1b1148c178f91c
Laka Nodia <laka.nodia@gmail.com>
2023-04-07 11:04:57 +0400

    Add date control dashboard component

--- src/app/date-control/date-dashboard/date-dashboard.component.ts
+++ src/app/date-control/date-dashboard/date-dashboard.component.ts
@@ -8,12 +8,26 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class DateDashboardComponent implements OnInit {
   dateForm = new FormGroup({
-    day: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    day: new FormControl('', [Validators.required, Validators.maxLength(2)]),
     month: new FormControl('', Validators.required),
     year: new FormControl('', Validators.required),
   });
 
+  get day() {
+    return this.dateForm.get('day') as FormControl;
+  }
+
+  get month() {
+    return this.dateForm.get('month') as FormControl;
+  }
+
+  get year() {
+    return this.dateForm.get('year') as FormControl;
+  }
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.day.valid);
+  }
 }
